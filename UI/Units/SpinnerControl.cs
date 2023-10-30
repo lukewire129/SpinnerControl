08b7fa2de8c5115af09e23e470d8f6128ef4e2d8
lukewire <lukewire129@gmail.com>
2023-10-31 00:09:03 +0900

    초기 커밋.

---
+++ UI/Units/SpinnerControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SpinnerControl.UI.Units
+{
+    public class SpinnerControl : ContentControl
+    {
+        static SpinnerControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SpinnerControl), new FrameworkPropertyMetadata(typeof(SpinnerControl)));
+        }
+    }
+}
