f11c4563ca9d6738789694262dcf40ef8e9f7983
lukewire <lukewire129@gmail.com>
2023-10-31 00:14:17 +0900

    Update .

---
+++ src/DemoApp/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Jamesnet.Wpf.Controls;
+
+namespace DemoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DarkThemeWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent ();
+        }
+    }
+}
---
+++ src/DemoApp/Starter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DemoApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public class Starter
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            new App ().Run ();
+        }
+    }
+}
